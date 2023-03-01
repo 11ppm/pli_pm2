9f2243dc0bae166f8f395dc843dd10dc20c1b337
11ppm <92081251+11ppm@users.noreply.github.com>
2023-03-02 02:24:39 +0900

    multi_lang

--- pm2process_restore.sh
+++ pm2process_restore.sh
@@ -5,8 +5,46 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-YELLOW='\033[1;33m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
+
+# CSVファイル名
+csv_file="./translations/translations.csv"
+
+# 言語の選択
+echo
+echo "Select a language 言語を選択してください :"
+echo
+echo "1. English"
+echo "2. 日本語"
+echo
+read -t30 -p "Enter the number 数字を入力してください : " lang_choice
+
+# 選択された言語に応じた列番号を設定
+case $lang_choice in
+1)
+    lang_col=2 # 英語列
+    ;;
+2)
+    lang_col=3 # 日本語列
+    ;;
+*)
+    echo "無効な選択です。"
+    exit 1
+    ;;
+esac
+
+# CSVファイルからメッセージを読み込み
+while read line; do
+    # キー名を取得
+    # cutコマンドで`|`を区切り文字とし、1列目のフィールドを抽出
+    key=$(echo $line | cut -d '|' -f 1)
+    # 選択された言語に対応するメッセージを取得
+    # awk -Fオプションを使ってフィールド区切り文字を`|`に指定、$で$lang_colで指定された列を取得
+    message=$(echo $line | awk -F '|' '{print $'$lang_col'}')
+    # 変数に格納
+    eval "$key=\"$message\""
+done <$csv_file
 
 # 変数定義
 backup_dir="/plinode_backups" # バックアップが格納されたディレクトリ
@@ -19,7 +57,7 @@ start=0                       # 表示開始番号
 echo
 echo -e "${YELLOW}#########################################################################"
 echo -e "${YELLOW}"
-echo -e "${YELLOW}                    Displaying list of backup files."
+echo -e "${YELLOW}                    $RESTORE_DISPLAY_LIST"
 echo -e "${YELLOW}"
 echo -e "${YELLOW}#########################################################################${NC}"
 echo
@@ -33,7 +71,7 @@ while true; do
         # 一覧表示の際にファイル別に色分け
         filename=${files[i]}
         color=""
-        if [[ "$filename" =~ "pm2" ]]; then
+        if [[ "$filename" =~ "pm2process" ]]; then
             color=$RED
         elif [[ "$filename" =~ "db" ]] || [[ "$filename" =~ "conf" ]]; then
             color=$BLUE
@@ -43,62 +81,79 @@ while true; do
         # 色分け状態で一覧表示
         echo -e "$((i + 1)). $color${files[i]}$NC"
     done
-    # 表示リスト頁の最終番号が全バックアップファイル数に達した時
+
+    # 表示リスト最終ページ
+
     if [ $end -eq $((file_num - 1)) ]; then
         echo
-        echo -e " All backup files have been displayed."
+        echo -e "$ALL_BACKUP_FILES_DISPLAYED"
+        read -t30 -r -p "$RESTORE_ENTER_FILE_NUMBER : " input
+    else
+        echo
+        echo "$RESTORE_DISPLAY_NEXT_PAGE"
+        read -t30 -r -p "$RESTORE_ENTER_FILE_NUMBER : " input
     fi
-    # 次の頁&番号入力
-    echo
-    echo "Enter 'n' to display next page."
-    read -t30 -r -p "Enter the number of the file you want to restore. (q to cancel) : " input
-    # 終了ステータス
+
+    # ちょっと切り離し
+
+    # $? は終了ステータス
     if [ $? -gt 128 ]; then
         echo
         echo
-        echo -e "${PURPLE}      No response for 30 seconds, session will end.${NC}"
+        echo -e "${PURPLE}      $NO_RESPONSE_SESSION_END${NC}"
         echo
         break
-    fi
-    echo
-    if [ "$input" == "q" ] || [ "$input" == "Q" ]; then
+    elif [ "$input" == "q" ] || [ "$input" == "Q" ]; then
         break
-    fi
-    if [ "$input" == "n" ] || [ "$input" == "N" ]; then
+    elif [ "$input" == "n" ] || [ "$input" == "N" ]; then
+        echo
         start=$((end + 1))
         continue
+    # else
+    #     echo "入力が無効です。再度入力してください。"
     fi
+
+
+
+    # ↓選択したファイルの確認
+
     # 変数inputに格納された値を変数file_indexとする
     file_index=$((input - 1))
     if [ $file_index -lt 0 ] || [ $file_index -ge $file_num ]; then
-        echo -e "${PURPLE} Invalid number input. Please input a valid number.${NC}"
+        echo
+        echo -e "${PURPLE}$RESTORE_INVALID_NUMBER${NC}"
         echo
         continue
     fi
     file=${files[file_index]}
     # ファイル名にpm2processが含まれているか判定
     if [[ $file != *"pm2process"* ]]; then
-        echo -e "${PURPLE} Note: choose pm2 process file.${NC}"
+        echo
+        echo -e "${PURPLE}$RESTORE_NOTE_CHOOSE_PM2${NC}"
         echo
         continue
     fi
-    echo "Restoring the following file.:"
-    echo $file
-    read -t30 -r -p "Are you okay with this ? (Y/N) : " confirm
+    echo
+    echo -e "$RESTORE_RESTORING_FOLLOWING_FILE: ${RED}$file${NC}"
+    echo
+    read -t30 -r -p "$RESTORE_ARE_YOU_OKAY [Y/N] : " confirm
     if [ $? -gt 128 ]; then
         echo
         echo
-        echo -e "${PURPLE}      No response for 30 seconds, session will end.${NC}"
+        echo -e "${PURPLE}      $NO_RESPONSE_SESSION_END${NC}"
         echo
         break
     fi
+
+    # ↓復元実行
+
     if [ "$confirm" == "Y" ] || [ "$confirm" == "y" ]; then
 
         # pm2の停止
         echo
         echo -e "${YELLOW}#########################################################################"
         echo -e "${YELLOW}"
-        echo -e "${YELLOW}                         Stopping pm2 process."
+        echo -e "${YELLOW}                         $RESTORE_STOPPING_PM2_PROCESS"
         echo -e "${YELLOW}"
         echo -e "${YELLOW}#########################################################################${NC}"
         echo
@@ -109,7 +164,7 @@ while true; do
         echo
         echo -e "${YELLOW}#########################################################################"
         echo -e "${YELLOW}"
-        echo -e "${YELLOW}                    Starting PM2 process restoration."
+        echo -e "${YELLOW}                    $RESTORE_STARTING_PM2_RESTORATION"
         echo -e "${YELLOW}"
         echo -e "${YELLOW}#########################################################################${NC}"
         echo
@@ -119,8 +174,8 @@ while true; do
         echo
         echo -e "${YELLOW}#########################################################################"
         echo -e "${YELLOW}"
-        echo -e "${YELLOW}                    PM2 process restoration complete."
-        echo -e "${YELLOW}                    Next, restart the pm2 service." # restart pm2 service
+        echo -e "${YELLOW}                    $RESTORE_PM2_RESTORATION_COMPLETE"
+        echo -e "${YELLOW}                    $RESTORE_NEXT_RESTART_PM2_SERVICE" # restart pm2 service
         echo -e "${YELLOW}"
         echo -e "${YELLOW}#########################################################################${NC}"
         echo
@@ -139,7 +194,7 @@ while true; do
         echo
         echo -e "${YELLOW}#########################################################################"
         echo -e "${YELLOW}"
-        echo -e "${YELLOW}                 Successful restoration of pm2 process."
+        echo -e "${YELLOW}                 $RESTORE_SUCCESSFUL_RESTORATION"
         echo -e "${YELLOW}"
         echo -e "${YELLOW}#########################################################################${NC}"
         echo
@@ -147,16 +202,51 @@ while true; do
     # fi
     elif [ "$confirm" == "N" ] || [ "$confirm" == "n" ]; then
         echo
-        echo -e "${PURPLE}      Restoration cancelled.${NC}"
+        echo -e "${PURPLE}      $RESTORE_RESTORATION_CANCELLED${NC}"
         echo
         break
     else
         echo
-        echo -e "${PURPLE}      Invalid input.${NC}"
+        echo -e "${PURPLE}      $INVALID_INPUT${NC}"
         echo
-        echo -e "Enter the number of the file you want to restore. : "
-        echo -e "Enter 'n' to display next page (q to cancel) :  "
+        echo -e "$RESTORE_ENTER_FILE_NUMBER : "
+        echo -e "$RESTORE_ENTER_N_TO_DISPLAY_NEXT_PAGE :  "
         echo
     fi
     start=$((file_index + 1))
 done
+
+# echo "$key"
+# echo "$BACKUP"
+# echo "$START_BACKUP_PM2"
+# echo "$STOP_PM2_FIRST"
+# echo "$START_BACKUP_PM2_AGAIN"
+# echo "$SUCCESSFUL_BACKUP"
+# echo "$PM2_STOPPED_RESTART"
+# echo "$NO_RESPONSE_SESSION_END"
+# echo "$PM2_REMAINS_STOPPED"
+# echo "$RESTART_PM2"
+# echo "$INVALID_INPUT"
+# echo "$DISPLAY_BACKUP_CONTENTS"
+# echo "$DISPLAY_BACKUP_FILE"
+# echo "$ALL_BACKUP_FILES_DISPLAYED"
+# echo "$PRESS_N_NOT_TEN_SHOWN"
+# echo "$NO_RESPONSE_SESSION_END_AGAIN"
+# echo "$ENDING_SESSION"
+# echo "$INVALID_INPUT_BESIDES_N_Q"
+# echo "$RESTORE"
+# echo "$RESTORE_DISPLAY_LIST"
+# echo "$RESTORE_DISPLAY_NEXT_PAGE"
+# echo "$RESTORE_ENTER_FILE_NUMBER"
+# echo "$RESTORE_INVALID_NUMBER"
+# echo "$RESTORE_NOTE_CHOOSE_PM2"
+# echo "$RESTORE_RESTORING_FOLLOWING_FILE"
+# echo "$RESTORE_ARE_YOU_OKAY"
+# echo "$RESTORE_STOPPING_PM2_PROCESS"
+# echo "$RESTORE_STARTING_PM2_RESTORATION"
+# echo "$RESTORE_PM2_RESTORATION_COMPLETE"
+# echo "$RESTORE_NEXT_RESTART_PM2_SERVICE"
+# echo "$RESTORE_SUCCESSFUL_RESTORATION"
+# echo "$RESTORE_RESTORATION_CANCELLED"
+# echo "$RESTORE_ENTER_FILE_NUMBER_AGAIN"
+# echo "$RESTORE_ENTER_N_TO_DISPLAY_NEXT_PAGE"
